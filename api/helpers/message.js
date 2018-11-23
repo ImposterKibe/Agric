165fef0890485b872ff7ec8fc440b9de809c7d79
ImposterKibe <kibestevemwa@gmail.com>
2018-11-23 12:24:46 +0300

    Added Voice.js,

---
+++ api/helpers/message.js
@@ -0,0 +1,81 @@
+require('dotenv').config()
+// We need this to build our post string
+const querystring = require('querystring');
+const https       = require('https');
+
+const options = {
+    userName:process.env.AFRICAS_TALKING_USERNAME,
+    apikey: process.env.AFRICAS_TALKING_API_KEY
+}
+const AfricasTalking = require('africastalking')(options)
+// Your login credentials
+
+const axios = require('axios')
+
+const recieveMessage = async() =>{
+    
+
+}
+ 
+const sendMessage= () =>{
+    const to = '+254706229743'   
+    const message = "Hi, Have a safe journey today";
+
+    const post_data = querystring.stringify ({
+        'username' : userName,
+        'to'       : to,
+        'message'  : message
+     });
+
+    const post_options = {      
+        host   : 'api.sandbox.africastalking.com',
+        path   : '/version1/messaging',
+        method : 'POST',
+
+        rejectUnauthorized : false,
+        requestCert        : true,
+        agent              : false,
+                       
+        headers: {
+                           'Content-Type' : 'application/x-www-form-urlencoded',
+                            'Content-Length': post_data.length,
+                            'Accept': 'application/json',
+                            'apikey': apikey
+                        }
+                    };
+
+
+
+    const post_req = https.request(post_options, (res)=> {
+        res.setEncoding('utf8')
+        res.on('data', (chunk) => {
+            console.log("chunk_Output: ",chunk)
+            const jsObject   = JSON.parse(chunk)
+            const recipients = jsObject.SMSMessageData.Recipients
+            
+            if ( recipients.length > 0 ) {
+                for (let i = 0; i < recipients.length; ++i ) {
+                     let logStr  = 'number=' + recipients[i].number
+                     logStr     += ';cost='   + recipients[i].cost
+                     logStr     += ';status=' + recipients[i].status // status is either "Success" or "error message"
+                     logStr     += ';statusCode=' + recipients[i].statusCode
+                       console.log(logStr)
+                            }
+                        } else {
+                            console.log('Error while sending: ' + jsObject.SMSMessageData.Message)
+                    }
+                })
+            })
+            
+            // Add post parameters to the http request
+            post_req.write(post_data)
+            
+            post_req.end()
+        }
+        //Call sendMessage method
+        sendMessage()
+
+        module.exports={
+            sendMessage,
+            recieveMessage
+        }
---
+++ api/helpers/voice.js
@@ -0,0 +1,27 @@
+const axios = require('axios')
+const options = {
+    apiKey: process.env.AFRICAS_TALKING_API_KEY,
+    username: process.env.AFRICAS_TALKING_USERNAME 
+}
+const AfricasTalking = require('africastalking')(options)
+const voice = AfricasTalking.VOICE
+
+const call =(req)=>{ 
+    voice.call({
+        callFrom: process.env.VIRTUAL_PHONE_NUMBER,
+        callTo: '+254726123456'
+    }).then(callData=>{
+        return callData
+    }).catch(err=>{
+        return err
+    })
+}
+
+const callCenter = (req)=>{
+    //persist call info
+
+    console.log(req.swagger.params)
+    const text = 'Thank you for calling'
+    const response = '<?xml version="1.0>" encoding="UTF-8"?><Response><Say>' +text + 
+     '</Say><Dial phoneNumbers="+2547062297423" ringbackTone="" record="false" sequential="true"/></Response>'
+}
